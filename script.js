01d677c5a7f92a5749f2bba75be8eb7a0b8b2ad2
Adam <surzyn.adam@gmail.com>
2023-02-13 00:34:54 +0100

    books array are added to the site when called

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /*
 ***TO DO******************************
-1)Add function that will take users input and store the input books into shelf array
-2)Wite a function that loops through the array and displays each book on the page
 3)Add a 'new book' button taht brings up a form and allows users to input details for the book they want to add.
 4)Add a button that will remove the book from display
 5)Add a button on a book that will change its display status
@@ -29,6 +27,28 @@ function addBookToShelf(book){
     shelf.push(book)
 }
 
+function addBooksToSite(shelf){
+
+    let booksContainer = document.getElementsByClassName("BooksCollectionContainer")[0]
+
+    shelf.forEach((book, i) =>{
+
+        let appendedBook = booksContainer.appendChild(document.createElement('div'))
+        appendedBook.className = `Book ${i.toString()}`
+        let newBook = booksContainer.getElementsByClassName(`Book ${i.toString()}`)[0]
+        let bookProperties = Object.keys(book)
+    
+        
+        bookProperties.forEach(property =>{
+    
+            let newchild = newBook.appendChild(document.createElement('li'))
+            newchild.innerHTML = book[property]
+    
+        }) 
+    
+    })
+}
+
 let bookTitle = prompt("Please enter the title of the book", "The Hobbit")
 let bookYear = prompt("Please enter the year in which the book was written", "1957")
 let bookAuthor = prompt("Please enter the author of the book", "JRR Tolkien")
@@ -42,23 +62,7 @@ const mobyDick = new Book("Moby Dick", "1900", "the one", "100", "false")
 addBookToShelf(theHobbit)
 addBookToShelf(mobyDick)
 
-//******************************************
-//Now have to create a book html element for each book on the shelf. Then display it in the element itself
-//******************************************
-
-let booksContainer = document.getElementsByClassName("Book")[0]
+addBooksToSite(shelf)
 
 
-shelf.forEach(book =>{
 
-    let bookProperties = Object.keys(book)
-
-    
-    bookProperties.forEach(property =>{
-
-        let newchild = booksContainer.appendChild(document.createElement('li'))
-        newchild.innerHTML = book[property]
-
-    }) 
-
-})
